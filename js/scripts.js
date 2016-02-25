65d3f73d14a40eaaec1a82884ebc030e66a2c7f1
Helena V. Icart <helenav.icart@gmail.com>
2016-02-25 13:50:27 +0100

    animation added

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ function scaleBannerVideoSize(element){
             typeSpeed: 10,
             showCursor: false,
             backDelay: 1500,
-          
+
             onStringTyped: function(){
               $(".load").css({"display":"inline-flex"});
 
@@ -78,113 +78,112 @@ function scaleBannerVideoSize(element){
 
 
 //scrolling stripes
-var vh = window.innerHeight,
-    dh = document.body.scrollHeight;
-
-var lastPos = 0;
-
-var stripes = [], stripes2 = [];
-var numStripes = Math.ceil(vh / 49),
-    numStripes2 = numStripes + 1;
-
-var h = vh / numStripes;
-
-var triggerPoint = document.getElementById('trigger').offsetTop;
-
-var topBottom;
-
-//init stripes
-for(var i = 0; i < numStripes; i++)
-{
-  var stripe = {};
-  stripe.h = 0;
-  stripe.t = h * i;
-
-  stripes.push(stripe);
-}
-for(var i = 0; i < numStripes2; i++)
-{
-  var stripe = {};
-  stripe.h = 0;
-  stripe.t = (h * i) - h/2;
-
-  stripes2.push(stripe);
-}
-
-window.onscroll = function()
-{
-  var scrollPos = window.scrollY;
-  var newH, newH2;
-
-  if(scrollPos <= triggerPoint)
-  {
-    topBottom = 'top';
-    newH = h - (triggerPoint - scrollPos) / numStripes;
-    newH2 = newH;
-
-    for(var j = 0; j < stripes.length; j++)
-    {
-      stripes[j].h = newH + j*1.5;
-    }
-
-    for(var a = 0; a < stripes2.length; a++)
-    {
-      stripes2[a].h = newH2 + a*1.5;
-    }
-  }
-  else if (scrollPos > triggerPoint)
-  {
-    topBottom = 'bottom'
-    newH = h - (scrollPos - triggerPoint) / numStripes;
-    newH2 = newH;
-
-    for(var j = 0; j < stripes.length; j++)
-    {
-      stripes[j].h = newH + j*1.5;
-    }
-
-    for(var b = 0; b < stripes2.length; b++)
-    {
-      stripes2[b].h = newH2 + b*1.5;
-    }
-  }
-
-  //draw stripes
-  var stripeContainer1 = document.getElementsByClassName('stripes-wrapper')[0],
-      stripeContainer2 = document.getElementsByClassName('stripes-wrapper2')[0];
-
-  stripeContainer1.innerHTML = '';
-  stripeContainer2.innerHTML = '';
-
-  for(var k = 0; k < stripes.length; k++)
-  {
-    var opacity;
-    if(scrollPos == 0 || scrollPos == dh-vh)
-    {
-      opacity = 0;
-    }
-    else
-    {
-      opacity = stripes[k].h / (h/2);
-    }
-
-    stripeContainer1.innerHTML = stripeContainer1.innerHTML + ('<div class="stripe" style="height: ' + stripes[k].h + 'px;' + topBottom + ':' + stripes[k].t + 'px; opacity: ' + opacity + '"><div style="' + topBottom + ': ' + (h - 49) / 2 + 'px"></div></div>');
-  }
-
-  for(var c = 0; c < stripes2.length; c++)
-  {
-    var opacity;
-    if(scrollPos == 0 || scrollPos == dh-vh)
-    {
-      opacity = 0;
-    }
-    else
-    {
-      opacity = stripes2[c].h / (h/2);
-    }
-
-    stripeContainer2.innerHTML = stripeContainer2.innerHTML + ('<div class="stripe" style="height: ' + stripes2[c].h + 'px;' + topBottom + ':' + stripes2[c].t + 'px; opacity: ' + opacity + '"><div style="' + topBottom + ': ' + (h - 49) / 2 + 'px"></div></div>');
-  }
-
-  lastPos = scrollPos;
-};
+// var vh = window.innerHeight,
+//     dh = document.body.scrollHeight;
+//
+// var lastPos = 0;
+//
+// var stripes = [], stripes2 = [];
+// var numStripes = Math.ceil(vh / 49),
+//     numStripes2 = numStripes + 1;
+//
+// var h = vh / numStripes;
+//
+// var triggerPoint = document.getElementById('trigger').offsetTop;
+//
+// var topBottom;
+//
+// //init stripes
+// for(var i = 0; i < numStripes; i++)
+// {
+//   var stripe = {};
+//   stripe.h = 0;
+//   stripe.t = h * i;
+//
+//   stripes.push(stripe);
+// }
+// for(var i = 0; i < numStripes2; i++)
+// {
+//   var stripe = {};
+//   stripe.h = 0;
+//   stripe.t = (h * i) - h/2;
+//
+//   stripes2.push(stripe);
+// }
+//
+// window.onscroll = function()
+// {
+//   var scrollPos = window.scrollY;
+//   var newH, newH2;
+//
+//   if(scrollPos <= triggerPoint)
+//   {
+//     topBottom = 'top';
+//     newH = h - (triggerPoint - scrollPos) / numStripes;
+//     newH2 = newH;
+//
+//     for(var j = 0; j < stripes.length; j++)
+//     {
+//       stripes[j].h = newH + j*1.5;
+//     }
+//
+//     for(var a = 0; a < stripes2.length; a++)
+//     {
+//       stripes2[a].h = newH2 + a*1.5;
+//     }
+//   }
+//   else if (scrollPos > triggerPoint)
+//   {
+//     topBottom = 'bottom'
+//     newH = h - (scrollPos - triggerPoint) / numStripes;
+//     newH2 = newH;
+//
+//     for(var j = 0; j < stripes.length; j++)
+//     {
+//       stripes[j].h = newH + j*1.5;
+//     }
+//
+//     for(var b = 0; b < stripes2.length; b++)
+//     {
+//       stripes2[b].h = newH2 + b*1.5;
+//     }
+//   }
+//
+//   //draw stripes
+//   var stripeContainer1 = document.getElementsByClassName('stripes-wrapper')[0],
+//       stripeContainer2 = document.getElementsByClassName('stripes-wrapper2')[0];
+//
+//   stripeContainer1.innerHTML = '';
+//   stripeContainer2.innerHTML = '';
+//
+//   for(var k = 0; k < stripes.length; k++){
+//     var opacity;
+//     if(scrollPos == 0 || scrollPos == dh-vh)
+//     {
+//       opacity = 0;
+//     }
+//     else
+//     {
+//       opacity = stripes[k].h / (h/2);
+//     }
+//
+//     stripeContainer1.innerHTML = stripeContainer1.innerHTML + ('<div class="stripe" style="height: ' + stripes[k].h + 'px;' + topBottom + ':' + stripes[k].t + 'px; opacity: ' + opacity + '"><div style="' + topBottom + ': ' + (h - 49) / 2 + 'px"></div></div>');
+//   }
+//
+//   for(var c = 0; c < stripes2.length; c++)
+//   {
+//     var opacity;
+//     if(scrollPos == 0 || scrollPos == dh-vh)
+//     {
+//       opacity = 0;
+//     }
+//     else
+//     {
+//       opacity = stripes2[c].h / (h/2);
+//     }
+//
+//     stripeContainer2.innerHTML = stripeContainer2.innerHTML + ('<div class="stripe" style="height: ' + stripes2[c].h + 'px;' + topBottom + ':' + stripes2[c].t + 'px; opacity: ' + opacity + '"><div style="' + topBottom + ': ' + (h - 49) / 2 + 'px"></div></div>');
+//   }
+//
+//   lastPos = scrollPos;
+// };
